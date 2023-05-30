821b998038bbe260c91f84dd5992c4cda3cf061a
Mian Xiong <mianxion@usc.edu>
2023-05-29 18:54:23 -0700

    Add one more solution to 0168

--- 0168/Solution.java
+++ 0168/Solution.java
@@ -1,18 +1,32 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
+        // Solution 1:
+        // String res = "";
+        // if(columnNumber == 1)
+        //     return "A";
+        // while(columnNumber != 0) {
+        //     char tmp = 'A';
+        //     tmp += (columnNumber - 1) % 26; // as A -> 1 rather than 0
+        //     res = tmp + res;
+        //     if(columnNumber == 26) {
+        //         columnNumber = 0;
+        //         break;
+        //     }
+        //     columnNumber = (columnNumber - (tmp - 'A' + 1)) / 26;
+        // }
+        // return res;
+
+        // Solution 1 plus: optimize based on solution 1 
         String res = "";
-        if(columnNumber == 1)
-            return "A";
-        while(columnNumber != 0) {
+        while(columnNumber > 26) {
             char tmp = 'A';
             tmp += (columnNumber - 1) % 26; // as A -> 1 rather than 0
             res = tmp + res;
-            if(columnNumber == 26) {
-                columnNumber = 0;
-                break;
-            }
             columnNumber = (columnNumber - (tmp - 'A' + 1)) / 26;
         }
+        char tmp = 'A';
+        tmp += columnNumber % 27 - 1;
+        res = tmp + res;
         return res;
     }
 }
