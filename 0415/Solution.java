b9c58a61186dd9df6dea4ab5e5826c7362656194
Mian Xiong <mianxion@usc.edu>
2023-06-01 23:26:19 -0700

    Add one more solution to 0415

--- 0415/Solution.java
+++ 0415/Solution.java
@@ -1,5 +1,7 @@
 class Solution {
     public String addStrings(String num1, String num2) {
+        // Solution 1: Reverse the two original strings, and then calculate,
+        // and reverse the calculated result to get the final result
         String reverse1 = new StringBuffer(num1).reverse().toString();
         String reverse2 = new StringBuffer(num2).reverse().toString();
         int len_diff = (num1.length() > num2.length()) ? (num1.length() - num2.length()) : (num2.length() - num1.length());
@@ -26,5 +28,22 @@ class Solution {
             res += tmp;
         }
         return new StringBuffer(res).reverse().toString();
+
+        // Solution 2: Use two pointers, move from back to front
+        // int p1 = num1.length() - 1;
+        // int p2 = num2.length() - 1;
+        // String res = "";
+        // int cb = 0; // carry bit
+        // while(p1 >= 0 || p2 >= 0) {
+        //     int tmp1 = (p1 >= 0) ? (num1.charAt(p1) - '0') : 0;
+        //     int tmp2 = (p2 >= 0) ? (num2.charAt(p2) - '0') : 0;
+        //     res = (tmp1 + tmp2 + cb) % 10 + res;
+        //     cb = (tmp1 + tmp2 + cb) / 10;
+        //     p1--;
+        //     p2--;
+        // }
+        // if(cb != 0)
+        //     res = cb + res;
+        // return res;
     }
 }
