916e3d4bca35b9bc842094aade5415dd1757e732
Mian Xiong <mianxion@usc.edu>
2023-06-05 21:59:33 -0700

    Add one more solution to 0226

--- 0226/Solution.java
+++ 0226/Solution.java
@@ -15,38 +15,48 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        List<TreeNode> pendList = new ArrayList<TreeNode>();
+        // Solution 1: Solve the problem recursively
         if(root == null)
             return null;
-        pendList.add(root);
-        int size = pendList.size(); // the size of pendList
-        while(size != 0) {
-            if(pendList.get(size - 1).left != null && pendList.get(size - 1).right != null) {
-                // add the two new nodes into pendList
-                pendList.add(pendList.get(size - 1).left);
-                pendList.add(pendList.get(size - 1).right);
-                // invert the left node and the right node
-                TreeNode tmp = pendList.get(size - 1).left;
-                pendList.get(size - 1).left = pendList.get(size - 1).right;
-                pendList.get(size - 1).right = tmp;
-            } else if(pendList.get(size - 1).left != null) { // no right node
-                // add the left node into pendList
-                pendList.add(pendList.get(size - 1).left);
-                // invert the left node and the right node
-                pendList.get(size - 1).right = pendList.get(size - 1).left;
-                pendList.get(size - 1).left = null;
-            } else if(pendList.get(size - 1).right != null) { // no left node
-                // add the right node into pendList
-                pendList.add(pendList.get(size - 1).right);
-                // invert the left node and the right node
-                pendList.get(size - 1).left = pendList.get(size - 1).right;
-                pendList.get(size - 1).right = null;
-            }
-            // remove the current root node from the pendList
-            pendList.remove(size - 1);
-            // update the size of pendList
-            size = pendList.size();
-        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
         return root;
+
+        // Solution 2: Use while loop to solve
+        // List<TreeNode> pendList = new ArrayList<TreeNode>();
+        // if(root == null)
+        //     return null;
+        // pendList.add(root);
+        // int size = pendList.size(); // the size of pendList
+        // while(size != 0) {
+        //     if(pendList.get(size - 1).left != null && pendList.get(size - 1).right != null) {
+        //         // add the two new nodes into pendList
+        //         pendList.add(pendList.get(size - 1).left);
+        //         pendList.add(pendList.get(size - 1).right);
+        //         // invert the left node and the right node
+        //         TreeNode tmp = pendList.get(size - 1).left;
+        //         pendList.get(size - 1).left = pendList.get(size - 1).right;
+        //         pendList.get(size - 1).right = tmp;
+        //     } else if(pendList.get(size - 1).left != null) { // no right node
+        //         // add the left node into pendList
+        //         pendList.add(pendList.get(size - 1).left);
+        //         // invert the left node and the right node
+        //         pendList.get(size - 1).right = pendList.get(size - 1).left;
+        //         pendList.get(size - 1).left = null;
+        //     } else if(pendList.get(size - 1).right != null) { // no left node
+        //         // add the right node into pendList
+        //         pendList.add(pendList.get(size - 1).right);
+        //         // invert the left node and the right node
+        //         pendList.get(size - 1).left = pendList.get(size - 1).right;
+        //         pendList.get(size - 1).right = null;
+        //     }
+        //     // remove the current root node from the pendList
+        //     pendList.remove(size - 1);
+        //     // update the size of pendList
+        //     size = pendList.size();
+        // }
+        // return root;
     }
 }
