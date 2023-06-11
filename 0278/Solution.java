4d7dfab97026fa454d4e3be1c23cb6cc7c6e4550
Mian Xiong <mianxion@usc.edu>
2023-06-10 22:37:18 -0700

    Add one more solution to 0278

--- 0278/Solution.java
+++ 0278/Solution.java
@@ -6,19 +6,32 @@ public class Solution extends VersionControl {
         // The range of data that type int can represent is -2^31 ~ 2^31 - 1
         // The range of data that type long can represent is -2^63 ~ 2^63 - 1
         // ph + pt can be greater than the maximum value that type int can represent
+        // Solution 1: Binary search to find the minimum index whose isBadVersion() is false
+        // long ph = 1;
+        // long pt = n;
+        // int res = 0;
+        // while(ph <= pt) {
+        //     int mid = (int)((ph + pt) / 2);
+        //     if(isBadVersion(mid)) {
+        //         res = mid;
+        //         pt = mid - 1;
+        //     } else {
+        //         ph = mid + 1;
+        //     }
+        // }
+        // return res;
+
+        // Solution 2: A variation of solution 1
         long ph = 1;
         long pt = n;
-        int res = 0;
-        while(ph <= pt) {
+        while(ph < pt) { // DO NOT write <= since it will enter dead loop
             int mid = (int)((ph + pt) / 2);
-            System.out.println(mid);
             if(isBadVersion(mid)) {
-                res = mid;
-                pt = mid - 1;
+                pt = mid; // different from what solution 1 did
             } else {
                 ph = mid + 1;
             }
         }
-        return res;
+        return (int)pt;
     }
 }
