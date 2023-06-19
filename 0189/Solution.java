094580cc878b4ef2b7868bd1665506bc9fc36fd9
Mian Xiong <mianxion@usc.edu>
2023-06-19 01:15:47 -0700

    Add one more solution to 0189

--- 0189/Solution.java
+++ 0189/Solution.java
@@ -11,16 +11,49 @@ class Solution {
         // }
 
         // Solution 2: Create a new array to store the last k-elements
-        k = k % nums.length; // because it is possible that k > nums.length
-        int[] kele = new int[k]; // store the last k-elements into array kele
-        for(int i = 0; i < k; i++) {
-            kele[i] = nums[nums.length - (k - i)];
+        // k = k % nums.length; // because it is possible that k > nums.length
+        // int[] kele = new int[k]; // store the last k-elements into array kele
+        // for(int i = 0; i < k; i++) {
+        //     kele[i] = nums[nums.length - (k - i)];
+        // }
+        // for(int i = nums.length - k - 1; i >= 0; i--) {
+        //     nums[i + k] = nums[i];
+        // }
+        // for(int i = 0; i < k; i++) {
+        //     nums[i] = kele[i];
+        // }
+
+        // Solution 3: Reverse the two parts of the array and reverse the whole array (the pattern of rotating)
+        k = k % nums.length;
+        // reverse the first part of the array (the first (nums.length - k) elements)
+        int ph = 0;
+        int pt = nums.length - k - 1;
+        while(ph < pt) {
+            int tmp = nums[ph];
+            nums[ph] = nums[pt];
+            nums[pt] = tmp;
+            ph++;
+            pt--;
         }
-        for(int i = nums.length - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
+        // reverse the second part of the array (the last k elements)
+        ph = nums.length - k;
+        pt = nums.length - 1;
+        while(ph < pt) {
+            int tmp = nums[ph];
+            nums[ph] = nums[pt];
+            nums[pt] = tmp;
+            ph++;
+            pt--;
         }
-        for(int i = 0; i < k; i++) {
-            nums[i] = kele[i];
+        // reverse the whole array (all the nums.length elements)
+        ph = 0;
+        pt = nums.length - 1;
+        while(ph < pt) {
+            int tmp = nums[ph];
+            nums[ph] = nums[pt];
+            nums[pt] = tmp;
+            ph++;
+            pt--;
         }
     }
 }
