d6aa2bd7f4544cd1e4949d90944f18154d1aaf67
Mian Xiong <mianxion@usc.edu>
2023-06-21 13:48:10 -0700

    Add one more solution to 0090

--- 0090/Solution.java
+++ 0090/Solution.java
@@ -1,15 +1,43 @@
 class Solution {
+    // Solution 1: Keep adding one 
+    // public List<List<Integer>> subsetsWithDup(int[] nums) { 
+    //     Arrays.sort(nums);
+    //     List<List<Integer>> res = new ArrayList<>();
+    //     int[] binary = new int[nums.length];
+    //     binary[0] = -1;
+    //     int count = 1;
+    //     while(count <= Math.pow(2, nums.length)) {
+    //         addOne(binary); // 1 - 2^n
+    //         List<Integer> current = new ArrayList<>();
+    //         for(int i = 0; i < nums.length; i++) {
+    //             if(binary[i] != 0)
+    //                 current.add(nums[i]);
+    //         }
+    //         if(!res.contains(current))
+    //             res.add(current);
+    //         count++;
+    //     }
+    //     return res;
+    // }
+
+    // public void addOne(int[] binary) {
+    //     int pos = 0;
+    //     binary[pos]++;
+    //     while(binary[pos] == 2) {
+    //         binary[pos++] = 0;
+    //         binary[pos]++;
+    //     }
+    // }
+
+    // Solution 2: Implement via shifting operation
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        int[] binary = new int[nums.length];
-        binary[0] = -1;
-        int count = 1;
-        while(count <= Math.pow(2, nums.length)) {
-            addOne(binary); // 1 - 2^n
+        int count = 0;
+        while(count < Math.pow(2, nums.length)) { // 0 ~ 2^n - 1
             List<Integer> current = new ArrayList<>();
-            for(int i = 0; i < nums.length; i++) {
-                if(binary[i] != 0)
+            for(int i = 0; i < nums.length; i++) { // the number of digits is (nums.length)
+                if((count & (1 << i)) != 0) // Note that the priority of & is lower than !=
                     current.add(nums[i]);
             }
             if(!res.contains(current))
@@ -17,14 +45,5 @@ class Solution {
             count++;
         }
         return res;
-    }
-
-    public void addOne(int[] binary) {
-        int pos = 0;
-        binary[pos]++;
-        while(binary[pos] == 2) {
-            binary[pos++] = 0;
-            binary[pos]++;
-        }
     }
 }
