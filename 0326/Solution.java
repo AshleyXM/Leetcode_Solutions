4b2f0860e0dd77f9aba680150ff3da40366fbdf7
Mian Xiong <mianxion@usc.edu>
2023-08-31 17:19:03 -0700

    The solution to 0326

---
+++ 0326/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        while(n % 3 == 0 && n != 0)
+            n /= 3;
+        if(n == 1) return true;
+        else return false;
+    }
+}
