70461fa017e0603d926399b78b6c4796131e72f7
Mian Xiong <mianxion@usc.edu>
2023-09-08 13:32:36 -0700

    The solution to 0215

---
+++ 0215/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i = 0; i < nums.length; i++)
+            pq.offer(nums[i]);
+        for(int i = 0; i < k - 1; i++)
+            pq.poll();
+        return pq.peek();
+    }
+}
