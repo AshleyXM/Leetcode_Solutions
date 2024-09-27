3294787f3ad85ad3bacef3017f020b925cd76b95
AshleyXM <454198339@qq.com>
2024-09-27 13:36:33 -0700

    The solution to 0162

---
+++ 0162/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        int m = (l + r) / 2;
+        while(l < r) {
+            m = (l + r) / 2;
+            if(nums[m] > nums[m + 1]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
